fb7694ca21301308cbb0f78efc1da17844215038
elaineyeh <10456018@ntub.edu.tw>
2021-05-10 09:12:20 +0800

    refactor: model schema

--- src/ub/admin.py
+++ src/ub/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, ActivityCategory, Log, State, FastLink
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(ActivityCategory)
+admin.site.register(Log)
+admin.site.register(State)
+admin.site.register(FastLink)
--- src/ub/models.py
+++ src/ub/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 class User(models.Model):
-    fb_id = models.IntegerField(max_length=64)
-    state = models.ForeignKey("State", on_delete=models.SET_NULL, null=True, default=None)
-    google_token = models.TextField()
+    fb_id = models.TextField()
+    state = models.ForeignKey("State", on_delete=models.SET_NULL, null=True, default=None, blank=True)
+    google_token = models.TextField(null=True, blank=True)
     subscribe = models.BooleanField(default=False)
-    activity_category = models.ForeignKey("ActivityCategory", on_delete=models.SET_NULL, null=True, default=None)
+    activity_category = models.ForeignKey("ActivityCategory", on_delete=models.SET_NULL, null=True, default=None, blank=True)
 
 
 class FastLink(models.Model):
     name = models.TextField()
+    discription = models.TextField(null=True, blank=True)
+    button_label = models.TextField(null=True, blank=True)
     url = models.TextField()
 
 
@@ -19,8 +21,9 @@ class Log(models.Model):
 
 class State(models.Model):
     name = models.CharField(max_length=128)
-    parent = models.ForeignKey("State", on_delete=models.CASCADE)
-    function = models.CharField(max_length=64)
+    label = models.TextField(null=True, blank=True)
+    parent = models.ForeignKey("State", on_delete=models.CASCADE, null=True, blank=True)
+    function = models.CharField(max_length=64, blank=True)
     is_input = models.BooleanField(default=False)
 
 
